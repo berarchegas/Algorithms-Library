b69d3bc007a04ce0391d2d53ac3552813f2d97aa
Luiz Henrique Yuji Delgado Oda <247255@dac.unicamp.br>
2022-09-15 12:12:32 -0300

    add Coordinate Compression

---
+++ Structures/CoordinateCompression.cpp
@@ -0,0 +1,20 @@
+// 0 indexed
+// if x is in arr, getL returns the compressed id of x and getR returns id + 1
+// getR is unnecessary most times
+
+
+template<class T>
+class CoordinateCompression {
+public:
+	CoordinateCompression(const vector<T> &a) {
+		arr = a;
+		sort(arr.begin(), arr.end());
+		arr.resize(unique(arr.begin(), arr.end()) - arr.begin());
+	}
+
+	int getL(T x) { return lower_bound(arr.begin(), arr.end(), x) - arr.begin(); }
+	int getR(T x) { return upper_bound(arr.begin(), arr.end(), x) - arr.begin(); }
+	int size() const { return (int) arr.size(); }
+private:
+	vector<T> arr;
+};
